1207b1e1f9cfd1052c7d2f32474cf2c666d41abc
LiuXiangBiao <113957008+LiuXiangBiao@users.noreply.github.com>
2024-10-17 21:37:19 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import boto3
+
+def list_ec2_instances(access_key, secret_key):
+    session = boto3.Session(
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+    )
+    ec2 = session.resource('ec2')
+    
+    instances = ec2.instances.all()
+
+    print("Listing EC2 Instances:")
+    for instance in instances:
+        print(f'Instance ID: {instance.id}, State: {instance.state["Name"]}, Type: {instance.instance_type}')
+
+if __name__ == "__main__":
+  
+    ACCESS_KEY = ''
+    SECRET_KEY = ''
+    
+    list_ec2_instances(ACCESS_KEY, SECRET_KEY)
